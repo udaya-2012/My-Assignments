eb71b6924fa92031cea1911d222f5530d7efc0da
udaya-2012 <udayalive@gmail.com>
2025-10-16 22:49:54 +0530

    Add files via upload

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TestLeaf");
+		System.out.println("true");
+		System.out.println("false");
+		System.out.println("Seleniun");
+
+	}
+
+}
